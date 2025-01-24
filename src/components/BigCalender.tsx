3985155c3459a0c86487246a963370de7c822f9c
mayronnsalles <mayronncsalles@outlook.com>
2025-01-24 16:11:32 -0300

    paginas de parent e teacher finalizadas front-end

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
+import moment from 'moment'
+import { calendarEvents } from '@/lib/data'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useState } from 'react'
+
+
+// Configuração de localização no servidor
+import 'moment/locale/pt-br';
+moment.locale('pt-br');
+
+// Configuração de localização no cliente
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+const formattedDate = format(new Date(), 'MMMM yyyy', { locale: ptBR });
+
+const localizer = momentLocalizer(moment)
+
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  }
+  return (
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
+      onView={handleOnChangeView}
+      min={new Date (2025,1,0,8,0,0)}
+      max={new Date (2025,1,0,17,0,0)}
+      style={{ height: "98%" }}
+    />
+  )
+}
+export default BigCalendar
